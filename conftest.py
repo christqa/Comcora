04548cfad6bb59afdcf8638eddf9fc980cfe919a
christqa <chsargsyan@gmail.com>
2025-05-20 18:16:44 +0400

    updated yml

--- conftest.py
+++ conftest.py
@@ -24,16 +24,19 @@ def browser():
         options = webdriver.ChromeOptions()
 
         if HEADLESS:
-            options.add_argument("--headless=new")
+            options.add_argument("--headless")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
             options.add_argument("--remote-debugging-port=9222")
             options.add_argument("--window-size=1920,1080")
+            options.add_argument("--disable-software-rasterizer")
+            options.add_argument("--disable-features=VizDisplayCompositor")
 
-        # Use temp user data dir to avoid lock conflicts
-        temp_profile = tempfile.mkdtemp()
-        options.add_argument(f"--user-data-dir={temp_profile}")
+
+# Use temp user data dir to avoid lock conflicts
+        #temp_profile = tempfile.mkdtemp()
+        #options.add_argument(f"--user-data-dir={temp_profile}")
 
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
